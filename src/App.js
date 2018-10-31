f9539e76008004c7f0508877c6facce87628bdb0
Josh <jzeilmann93@gmail.com>
2018-10-31 18:11:31 -0500

    Navgiation edits

--- src/App.js
+++ src/App.js
@@ -1,15 +1,83 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import { Nav } from './components/Nav';
+import { About } from './components/About';
+import { Email } from './components/Email';
+import { Experiment } from './components/Experiment';
+import { LandingPages } from './components/LandingPages';
+import { Animation } from './components/Animation';
+
 
 class App extends Component {
+  state = {
+    view: 'about'
+  }
+  
+  handleClick = (e) => {
+    const id = e.target.id;
+    this.setState({
+      view: id
+    })
+  }
+
+//   handleScroll = (e) => {
+//     const views = ['about','email','expirement','landingpage']; 
+//     console.log(e.target.body.scrollTop);
+//     console.log(window.pageYOffset);
+//     if(window.pageYOffset > 80) {
+//       console.log('80')
+//       var newState = "email";
+//       this.setState({
+//         view: newState
+//       })
+//     }
+//     console.log('logging scroll')
+//     if (e.target.body.scrollTop < 1) {            
+//         this.setState({
+//           view: views[i]
+//         })
+//     } 
+//     else {
+//       console.log('Less than 100')
+//     }
+// }
+
+  componentDidMount = () => {
+    window.addEventListener('scroll', this.handleScroll, { passive: true })
+    
+  }
+
+  componentWillUnmount = () => {
+    window.removeEventListener('scroll', this.handleScroll)
+    console.log('Scroll event removed to window')
+  }
+
   render() {
+    console.log(this.state);
+    let view = this.state.view;
+    if ( view === 'about' ) {
+      view = <About />;
+    } else if (view === 'email') {
+      view = <Email />;
+    }
+    else if (view === 'experiment') {
+      view = <Experiment />;
+    }
+    else if (view === 'landingpages') {
+      view = <LandingPages  />;
+    }
+    else {
+      view = <Animation  />;
+    }
+
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <Nav onClick={this.handleClick} />
+          { view }
         </header>
-      </div>
+       </div>
     );
   }
 }
---
+++ src/components/Animation.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import mockup from '../assets/landing-page-mockups.png';
+import FadeIn from 'react-fade-in';
+
+export const Animation = () => {
+    return (
+        <FadeIn delay="100" transitionDuration="500">
+        <div className="view-email">
+            <div className="email-project">
+                <img className="email-mockup" src={mockup} alt="email-template" />
+            </div>
+            <div className="email-description">
+            <h1>We do Animation</h1>
+            <p>You can call it magic, but in reality there is no magic wand that can resolve our problems. The solution rests with our team of Creative Wizards’s hard work and discipline. They have developed a 3-step approach to systematically produce great design every single time.</p>
+            </div>
+        </div>
+        </FadeIn>
+    )
+}
